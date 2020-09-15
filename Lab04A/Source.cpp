6f8bd489939045b78ecd8ce2457c38258bffefd6
STEVEN MUHIRE <stevenmuhire69@outlook.com>
2020-09-15 13:43:52 -0600

    Added lab04a.cpp source code file

---
+++ Lab04A/Source.cpp
@@ -0,0 +1,34 @@
+//MUHIRE STEVEN NIYOYITA
+//september 15th 2020
+// Engr-1405-001
+#include <iostream>
+using namespace std;
+
+// This program will ask the user to input the number of gallons used and the number of miles traveled by a car and then calculate the the miles per gallon the car gets.
+
+int main()
+
+{
+	int gallons, miles, result;
+
+	cout << "Hello" << endl;
+	cout << "Miles per gallon calculator" << endl;
+
+	// get the number of gallons.
+	cout << "write the number of gallons" << endl;
+	cin >> gallons;
+
+	// get the number of miles.
+	cout << "write the number of miles" << endl;
+	cin >> miles;
+
+	// calculate the miles per gallon the car gets.
+	result = miles / gallons;
+
+	// the result of miles per gallon.;
+	cout << "The result is  " << result << endl;
+
+	return 0;
+
+	
+}
